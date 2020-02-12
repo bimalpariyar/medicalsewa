ef66ebb9f6d8114026d12b7a75c343354902ca3a
bimalpariyar <bimalpariyar1@gmail.com>
2020-02-12 16:13:46 +0545

    second day two

--- js/main.js
+++ js/main.js
@@ -28,4 +28,32 @@
       }
     }
   });
+
+  $(".featured__products__caro").owlCarousel({
+    responsiveClass: true,
+    nav: true,
+    smartSpeed: 500,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    margin: 30,
+    navText: [
+      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+      '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      1200: {
+        items: 4
+      }
+    }
+  });
 })(window.jQuery);
