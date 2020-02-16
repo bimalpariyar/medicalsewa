da0fbd7f4b29fcfd2c0d5763c8af293172a74db7
bimalpariyar <bimalpariyar1@gmail.com>
2020-02-16 16:51:17 +0545

    fifthday eve

--- js/main.js
+++ js/main.js
@@ -1,59 +1,80 @@
 (function($) {
   "use strict";
-  $(".home-slides").owlCarousel({
-    loop: true,
-    rewind: false,
-    responsiveClass: true,
-    nav: false,
-    smartSpeed: 500,
-    dots: true,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    navText: [
-      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-      '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-      0: {
-        items: 1
-      },
-      576: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      1200: {
-        items: 1
+  $(document).ready(function() {
+    $(".home-slides").owlCarousel({
+      loop: true,
+      rewind: false,
+      responsiveClass: true,
+      nav: false,
+      smartSpeed: 500,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+      ],
+      responsive: {
+        0: {
+          items: 1
+        },
+        576: {
+          items: 1
+        },
+        768: {
+          items: 1
+        },
+        1200: {
+          items: 1
+        }
       }
-    }
-  });
+    });
 
-  $(".featured__products__caro").owlCarousel({
-    responsiveClass: true,
-    nav: true,
-    smartSpeed: 500,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    margin: 30,
-    navText: [
-      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-      '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-      0: {
-        items: 1
-      },
-      576: {
-        items: 1
-      },
-      768: {
-        items: 3
-      },
-      1200: {
-        items: 4
+    $(".featured__products__caro").owlCarousel({
+      responsiveClass: true,
+      nav: true,
+      smartSpeed: 500,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      margin: 30,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+      ],
+      responsive: {
+        0: {
+          items: 1
+        },
+        576: {
+          items: 1
+        },
+        768: {
+          items: 3
+        },
+        1200: {
+          items: 4
+        }
       }
-    }
-  });
+    });
+    // Category drop
+    $(".header__bottom .categorymenu").click(function() {
+      $(".header__bottom .categorymenu__lists").slideToggle();
+      $(".categorymenu__head").toggleClass("siteBgColor");
+    });
+
+    // Product Detail Qty
+    let productVal = $(".produAddQty").val();
+    let currentValue = parseInt(productVal);
+    $(".prdQtyInc").click(function() {
+      $(".produAddQty").val(++currentValue);
+    });
+    $(".prdQtyDec").click(function() {
+      if (currentValue <= 1) {
+        return;
+      } else {
+        $(".produAddQty").val(--currentValue);
+      }
+    });
+  }); // Document Ready
 })(window.jQuery);
